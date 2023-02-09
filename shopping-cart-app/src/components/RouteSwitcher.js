39a0b1d946fda9ee3edcd7dfc44749c1a107a69d
James Graca-Jones <jamesgracajones@gmail.com>
2023-02-09 19:33:12 +0000

    Render a Home Route in RouteSwitcher
    Pass to it a websiteType of "shopping"
    Pass to it the src of a shopping-related image
    Give it a relative path of "/"

--- shopping-cart-app/src/components/RouteSwitcher.js
+++ shopping-cart-app/src/components/RouteSwitcher.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const routeComponentArray = [];
+import Home from "./Home";
+import shoppingImageSrc from "../images/shopping.jpg";
 
 function RouteSwitcher() {
     return (
         <div className="RouteSwitcher">
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={
+                        <Home
+                            websiteType="shopping"
+                            imageSrc={shoppingImageSrc}
+                        />
+                    } />
                 </Routes>
             </BrowserRouter>
         </div>
