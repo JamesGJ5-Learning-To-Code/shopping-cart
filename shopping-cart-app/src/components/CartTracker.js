25b89b76761027f38fd03b0889058a03e3169210
James Graca-Jones <jamesgracajones@gmail.com>
2023-02-09 20:47:26 +0000

    Render static CheckoutButton in CartTracker

--- shopping-cart-app/src/components/CartTracker.js
+++ shopping-cart-app/src/components/CartTracker.js
@@ -1,9 +1,11 @@
 import CartSizeDisplay from "./CartSizeDisplay";
+import CheckoutButton from "./CheckoutButton";
 
 function CartTracker() {
     return (
         <div className="CartTracker">
             <CartSizeDisplay />
+            <CheckoutButton />
         </div>
     )
 }
---
+++ shopping-cart-app/src/components/CheckoutButton.js
@@ -0,0 +1,9 @@
+function CheckoutButton() {
+    return (
+        <button className="CheckoutButton">
+            Pay
+        </button>
+    )
+}
+
+export default CheckoutButton;
