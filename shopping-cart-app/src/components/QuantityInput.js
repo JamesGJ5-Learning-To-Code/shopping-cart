b9ad811a6317a94d83d664ca6d29ace9ed8ef5da
James Graca-Jones <jamesgracajones@gmail.com>
2023-02-10 16:20:03 +0000

    Add TODO

--- shopping-cart-app/src/components/QuantityInput.js
+++ shopping-cart-app/src/components/QuantityInput.js
@@ -3,6 +3,7 @@ import uniqid from "uniqid";
 function QuantityInput({quantityChosen, indexOfQuantityChosen, forQuantityChosenChange}) {
     const inputID = uniqid();
     const handleChange = (event) => {
+        // TODO: imitate Amazon's input
         const newValue = event.target.value;
         if (newValue !== '') {
             const newQuantityChosen = parseInt(newValue);
