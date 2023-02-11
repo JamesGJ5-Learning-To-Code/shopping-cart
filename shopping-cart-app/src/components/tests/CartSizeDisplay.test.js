828fbf3a86cd603912fc0e97b4474f18ee07f893
James Graca-Jones <jamesgracajones@gmail.com>
2023-02-11 14:33:17 +0000

    Plan out test suite

---
+++ shopping-cart-app/src/components/tests/CartSizeDisplay.test.js
@@ -0,0 +1,15 @@
+// To be tested:
+    
+// - Incoming command messages effects with direct, public side-effects:
+//     - Specific:
+//         - Essentially, the creation of an CartSizeDisplay component should cause the rendering of what's in its return statement, this rendering being a public side-effect that is directly resulted by CartSizeDisplay
+//         - Make sure specific input props lead to the correct output (child) props
+//         - N.B.: not entirely sure if this fits the above definition but I think it works for now
+//     - Method of testing:         
+//         - Make assertions about said side-effects (the rendering)
+//         - Specifically, going to:
+//              - Try to render a CartSizeDisplay component with particular props passed to 
+//                it (say, a chosen integer for cartSize, e.g. 8)
+//              - Expect:
+//                  - getByText("There are 8 [get "8" via same const cartSize used 
+//                    for rendering] items in the cart!") toBeInTheDocument
